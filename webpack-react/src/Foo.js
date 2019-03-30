2f093d987b51466c9a91dc73169e92e7b9a63a25
rodel <rodelrebucas@gmail.com>
2019-03-30 23:28:04 +0800

    Added sample Lazy loading and code splitting setup

--- webpack-react/src/Foo.js
+++ webpack-react/src/Foo.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+// lazy loaded components via dynamic import from webpack. (Not supported on server side rendered app)
+const BarComponent = React.lazy(() => import("./Bar"));
 
 export default () => (
   <div>
-    <p>I am Foo! Pleasure to meet you.</p>
+    <Suspense fallback={<div>Loading Bar Component...</div>}>
+      <p>I am Foo! Pleasure to meet you.</p>
+      <BarComponent />
+    </Suspense>
   </div>
 );
