74f4dc6503c0e14072fc1643d977ed95363d2b39
rodel <rodel@vananaz.com>
2019-04-08 16:56:31 +0800

    use contenthash for longterm hashing

--- webpack-react/config/webpack.common.config.js
+++ webpack-react/config/webpack.common.config.js
@@ -18,7 +18,7 @@ module.exports = env => {
       // full ES2015+ environment
       entry: ["@babel/polyfill", APP_DIR], // support async/await
       output: {
-        filename: "[name].[chunkhash].js", // chunkhash instead of hash
+        filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "../dist")
       },
       module: {
